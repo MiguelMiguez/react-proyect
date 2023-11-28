2304baacbd1e600c35c33019ab344fe9aca95d12
Miguel Miguez <miguelmiguezangel@gmail.com>
2023-11-28 11:00:38 -0300

    update cart

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -11,7 +11,7 @@ const NavBar = () => {
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="collapse navbar-collapse flex-row-reverse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
@@ -26,7 +26,7 @@ const NavBar = () => {
                             <a class="nav-link active" aria-current="page" href="#">Accesories</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Cart</a>
+                            <a class="nav-link active" id='num' aria-current="page" href="#"><img className='CartBuy' src="https://cdn-icons-png.flaticon.com/512/3144/3144456.png" alt="" /> 5 </a>
                         </li>
                     </ul>
                 </div>
