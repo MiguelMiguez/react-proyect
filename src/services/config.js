3a368bbea70dab0a41efa87889404cdddb820f59
Miguel Miguez <138310942+MiguelMiguez@users.noreply.github.com>
2025-04-30 23:57:11 -0300

    update

--- src/services/config.js
+++ src/services/config.js
@@ -1,16 +1,15 @@
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCvy4SoZIT_IDKvgH-mmK170Ww4ynmCn0c",
   authDomain: "react-proyect-fd478.firebaseapp.com",
   projectId: "react-proyect-fd478",
-  storageBucket: "react-proyect-fd478.appspot.com",
+  storageBucket: "react-proyect-fd478.firebasestorage.app",
   messagingSenderId: "1021196762278",
   appId: "1:1021196762278:web:c8582e88859a435f014867",
-  measurementId: "G-49260T7PSR"
+  measurementId: "G-49260T7PSR",
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
